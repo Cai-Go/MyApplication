7a29fa78ea71531bbcefe568ad177334b8e30fa1
wuzhaohui <1158663782@qq.com>
2016-02-05 10:37:34 +0800

    文件存储 SharedPreferences存储 实现记住密码功能 SQLites实践 读取联系人 实现跨程序数据共享 Android Design Support Library实践：侧滑菜单，Tablayout,FloatingActionButton,TextInputLayout,Snackbar MD动画效果，Toolbar; Notification的使用以及一些效果; 接收和发送短信; 调用摄像头拍照和选择照片； 播放音频； 播放视频； 线程处理； Service; 后台执行的定时任务； WebView的使用；

---
+++ WebViewTest/src/main/java/com/example/webviewtest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.webviewtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);//让WebView支持JavaScript脚步
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);//根据传入的参数再去加载新的网页
+                return true;//表示当前WebView可以处理打开新网页的请求，不用借助系统浏览器
+            }
+        });
+        webView.loadUrl("http://wwww.baidu.com");
+    }
+}
